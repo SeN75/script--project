8cc3106400dd0ce84f866386e262d2b27966739a
saleh alsaggaf <saa111954@hotmail.com>
2023-04-17 23:14:32 +0300

    update: feedback endpoints done

--- server-side/src/routers/index.ts
+++ server-side/src/routers/index.ts
@@ -3,7 +3,8 @@ import { Router } from "express";
 import SubjectRouter from './subject';
 import LessonRouter from './lesson';
 import ContentRouter from './content'
-import ExerciseRouter from './exercise'
+import ExerciseRouter from './exercise';
+import FeedbackRouter from './feedback';
 const router = Router();
 
 
@@ -12,5 +13,6 @@ router.use('/api/subject', SubjectRouter);
 router.use('/api/lesson', LessonRouter);
 router.use('/api/content', ContentRouter);
 router.use('/api/exercise', ExerciseRouter)
+router.use('/api/feedback', FeedbackRouter)
 
 export default router
