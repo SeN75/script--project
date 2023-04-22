c44bca6a1dbf2a308d95710569e86f30ff5f8484
saleh alsaggaf <saa111954@hotmail.com>
2023-04-22 17:58:27 +0300

    update: login

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', loadChildren: () => import('./view/dashboard/dashboard.module').then(m => m.DashboardModule)},
-  {path: 'home', loadChildren: () => import('./view/main/main.module').then(m => m.MainModule)}
+  {path: 'home', loadChildren: () => import('./view/main/main.module').then(m => m.MainModule)},
+  {path: 'register', loadChildren: () => import('./view/register/register.module').then(m => m.MainModule)}
 ];
 
 @NgModule({
---
+++ client-side/src/app/view/register/pages/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  hide = true;
+  form = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [Validators.required])
+  })
+  get controls () {return this.form.controls}
+}
